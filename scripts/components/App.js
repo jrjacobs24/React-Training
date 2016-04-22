35eacb9dd7120ea4b16c5d9425f433dc385f1d30
JJ Jacobs <jjacobs@Javelin.local>
2016-04-22 17:41:53 -0400

    Initial commit

---
+++ scripts/components/App.js
@@ -0,0 +1,98 @@
+/*
+  App
+ */
+
+import React from 'react';
+import Catalyst from 'react-catalyst';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import Fish from './Fish';
+
+// Firebase
+import Rebase from 're-base';
+var base = Rebase.createClass('https://jj-bt-react-test.firebaseio.com/');
+
+var App = React.createClass({
+  mixins : [Catalyst.LinkedStateMixin],
+  getInitialState : function() {
+    return {
+      fishes : {},
+      order: {}
+    }
+  },
+  componentDidMount : function() {
+    // Sync data with Firebase
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
+
+    var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if(localStorageRef) {
+      // update our component state to reflect what is in localStore
+      this.setState({
+        order : JSON.parse(localStorageRef)
+      });
+    }
+  },
+  componentWillUpdate : function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
+  },
+  addToOrder : function(key){
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({ order : this.state.order });
+  },
+  removeFromOrder : function(key) {
+    delete this.state.order[key];
+    this.setState({
+      order : this.state.order
+    });
+  },
+  addFish : function(fish) {
+    var timestamp = (new Date()).getTime();
+    // update the state object
+    this.state.fishes['fish-' + timestamp] = fish;
+    // set the state
+    this.setState({ fishes : this.state.fishes });
+  },
+  removeFish : function(key) {
+    if (confirm("Are you sure you want to remove this fish?")) {
+      this.state.fishes[key] = null;
+      this.setState({
+        fishes : this.state.fishes
+      });
+    }
+  },
+  loadSamples : function() {
+    this.setState({
+      fishes : require('../sample-fishes')
+    });
+  },
+  renderFish : function(key) {
+    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+  },
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seasfood Market" />
+          <ul className="list-of-fishes">
+            {/*
+              Create array of fishes so we can loop through
+              and run the renderFish function on each item.
+            */}
+            {Object.keys(this.state.fishes).map(this.renderFish)}
+           
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder}/>
+        <Inventory addFish={this.addFish} removeFish={this.removeFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState}/>
+      </div>
+    )
+  }
+
+});
+
+export default App;
